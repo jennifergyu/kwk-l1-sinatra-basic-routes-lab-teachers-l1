6581f59c6c672ef9246be505cb93d176f76eb89a
Jennifer Yu <jyu81@illinois.edu>
2018-07-31 17:11:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+    get '/' do
+      "i love Bok Choy"
+end
+  get '/nutritious' do
+    "Has lots of fiber"
+  end
 end
